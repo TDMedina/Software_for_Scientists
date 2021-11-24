1e31949c9fb8155e03d538bb227f5d8cf5a7ac13
TDMedina <tylerdanmedina@gmail.com>
2021-11-24 10:24:07 +0000

    Initialize.

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+import sys
+import os
+
+def main(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Hi, {name}")
+    return os.EX_OK
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    # main("PyCharm")
+    sys.exit(main("Paul"))
